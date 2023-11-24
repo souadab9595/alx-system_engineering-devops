810fe2417f3a366c190ab300ea3feb0e67182a42
root <abid.souad28@gmail.com>
2023-11-24 20:53:05 +0000

    configuration_management

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/lib/python3.*/dist-packages/flask/__init__.py',
+  require => Package['python3-pip'],
+}
